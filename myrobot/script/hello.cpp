8f797f7cc04645e27106ed0db69c23a742bbf2e0
Xinzhou Jiang <92595395+likeyousmile@users.noreply.github.com>
2022-07-18 22:40:54 -0400

    Add files via upload

---
+++ myrobot/script/hello.cpp
@@ -0,0 +1,17 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Welcome to Xinzhou's World!\n");
+            }
+  //receive info from the world file
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
